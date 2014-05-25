b1dce5fa8c52fe31c945aefdefb0962cfe0defef
Paul Merrill <pdm@pdm.me>
2014-05-25 01:58:28 -0700

    outlier asteroids

--- RoidAvoid.spritebuilder/Asteroid.m
+++ RoidAvoid.spritebuilder/Asteroid.m
@@ -32,26 +32,38 @@
 	// Then you list which categories its allowed to collide with.
 	self.physicsBody.collisionMask = @[@"hero", @"planet",@"boundary",@"asteroid"];
 
+    if (frandom_range(0.0, 1.0) < ASTEROID_OUTLIER_PROB) {
+        [self setVarsForProfile:@"outlier"
+            minSize:ASTEROID_OUTLIER_SIZE_MIN            maxSize:ASTEROID_OUTLIER_SIZE_MAX
+            minSpeed:ASTEROID_OUTLIER_SPEED_MIN          maxSpeed:ASTEROID_OUTLIER_SPEED_MAX
+            minAngle:ASTEROID_OUTLIER_ANGLE_MIN          maxAngle:ASTEROID_OUTLIER_ANGLE_MAX
+            minAngVel:ASTEROID_OUTLIER_ANGLEVELOCITY_MIN maxAngVel:ASTEROID_OUTLIER_ANGLEVELOCITY_MAX];
+    }
+    else {
+        [self setVarsForProfile:@"normal"
+            minSize:ASTEROID_SIZE_MIN            maxSize:ASTEROID_SIZE_MAX
+            minSpeed:ASTEROID_SPEED_MIN          maxSpeed:ASTEROID_SPEED_MAX
+            minAngle:ASTEROID_ANGLE_MIN          maxAngle:ASTEROID_ANGLE_MAX
+            minAngVel:ASTEROID_ANGLEVELOCITY_MIN maxAngVel:ASTEROID_ANGLEVELOCITY_MAX];
+    }
+}
 
-    
-        
-    
-
-
-    float scale = frandom_range(ASTEROID_SIZE_MIN, ASTEROID_SIZE_MAX);
-    self.scale = scale;
-
-    float speed = frandom_range(ASTEROID_SPEED_MIN, ASTEROID_SPEED_MAX);
+- (void) setVarsForProfile:(NSString *)name
+        minSize:(float)minSize     maxSize:(float)maxSize
+        minSpeed:(float)minSpeed   maxSpeed:(float)maxSpeed
+        minAngle:(float)minAngle   maxAngle:(float)maxAngle
+        minAngVel:(float)minAngVel maxAngVel:(float)maxAngVel
+{
+    float scale = frandom_range(minSize, maxSize);
+    float speed = frandom_range(minSpeed, maxSpeed);
     float toEarth = -ccpToAngle(singleton.asteroidPos);
-    float randomFactor = frandom_range(ASTEROID_ANGLE_MIN, ASTEROID_ANGLE_MAX) * random() > 0 ? 1 : -1;
+    float randomFactor = frandom_range(minAngle, maxAngle) * random() > 0 ? 1 : -1;
     float angle = toEarth + randomFactor;
-    float angularVelocity = frandom_range(ASTEROID_ANGLEVELOCITY_MIN, ASTEROID_ANGLEVELOCITY_MAX);
-
+    float angularVelocity = frandom_range(minAngVel, maxAngVel) * random() > 0 ? 1 : -1;
+    
+    self.scale = scale;
     [self.physicsBody setVelocity:ccpMult(ccp(cosf(angle), sinf(angle)), speed)];
     [self.physicsBody setAngularVelocity:angularVelocity];
-
-    NSLog(@"Asteroid created with angle %f", angle);
-
 }
 
 @end
