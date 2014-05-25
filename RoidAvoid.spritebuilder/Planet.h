e0c6aa7712f78a9557338a82021f4f052ad78ef4
Paul Merrill <pdm@pdm.me>
2014-05-24 22:06:49 -0700

    forgot Planet.[hm]

---
+++ RoidAvoid.spritebuilder/Planet.h
@@ -0,0 +1,14 @@
+//
+//  Planet.h
+//  RoidAvoid
+//
+//  Created by Paul Merrill on 5/24/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface Planet : CCNode
+
+@end
